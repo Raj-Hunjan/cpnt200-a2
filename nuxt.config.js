e8ddbe427d8cd37f03baa35db44b0c0131dab4b3
Raj-Hunjan <rajhunjann@gmail.com>
2021-11-22 20:31:34 -0700

    dealing with errors

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,5 +50,19 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extend(config, ctx) {
+      // run eslint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node-modules)/,
+          options: {
+            fix: true
+          }
+        })
+      }
+    }
   }
 }
